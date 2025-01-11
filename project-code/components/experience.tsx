5712ebbaf8a2df2130e48d45b0c68c9679bc6eaa
Mann Anil Vora <mannvora19683@gmail.com>
2025-01-10 17:34:01 -0700

    Added experience section, re-factored the code and fixed ts issues

---
+++ project-code/components/experience.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "@/components/section-heading";
+import { VerticalTimeline } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import { VerticalElement } from "@/components/vertical-element";
+
+export default function Experience() {
+    const { ref } = useSectionInView({ sectionName: "Experience"});
+    return (
+        <section id="experience" ref={ref} className="mb-28 scroll-mt-28 sm:mb-40">
+        <SectionHeading>My experience</SectionHeading>
+        <VerticalTimeline lineColor="gray">
+            {experiencesData.map((item, index) => {
+            return (
+                <React.Fragment key={index}>
+                <VerticalElement
+                    title={item.title}
+                    description={item.description}
+                    date={item.date}
+                    icon={item.icon}
+                />
+                </React.Fragment>
+            );
+            })}
+        </VerticalTimeline>
+        </section>
+    );
+}
---
+++ project-code/components/vertical-element.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+
+type VerticalElementType = {
+  title: string;
+  description: string;
+  date: string;
+  icon: React.FunctionComponentElement<any>;
+};
+
+const VerticalElement = ({
+  title,
+  description,
+  date,
+  icon,
+}: VerticalElementType) => {
+  const theme = "light";
+  const { ref, inView } = useInView({ threshold: 0.25, triggerOnce: true });
+
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+        boxShadow: "none",
+        border: "1px solid rgba(0, 0, 0, 0.05)",
+        textAlign: "left",
+        padding: "1.3rem 2rem",
+      }}
+      contentArrowStyle={{
+        borderRight:
+          theme === "light"
+            ? "0.4rem solid #9ca3af"
+            : "0.4rem solid rgba(255, 255, 255, 0.5)",
+      }}
+      date={date}
+      icon={icon}
+      iconStyle={{
+        background: theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+        fontSize: "1.5rem",
+      }}
+      visible={inView}
+    >
+      <h3 className="font-semibold capitalize" ref={ref}>
+        {title}
+      </h3>
+      <p className="!mt-2 !font-normal text-gray-700">
+        {description}
+      </p>
+    </VerticalTimelineElement>
+  );
+};
+
+export { VerticalElement };
